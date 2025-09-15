e733dd5909ea1076f7c4f1945d05d7352ba4b49b
chaitra78 <53751398+chaitra78@users.noreply.github.com>
2025-09-15 15:45:52 +0530

    Update provider.tf

--- sg/provider.tf
+++ sg/provider.tf
@@ -5,17 +5,15 @@ terraform {
       version = "5.84.0"        #version of provider
     }
   }
-
   backend "s3" { #backend info should always be in terraform block
     bucket         = "my-s3-bucket-name-cr"
-    key            = "expense_k8_sg_module/terraform.tfstate"
-    region         = "us-east-1"   # region of bucket and dynamodb
+    key            = "terraform.tfstate"
+    region         = "us-east-1" # region of bucket and dynamodbsss
   }
 }
-
+ 
 # here we configure AWS provider
 provider "aws" {
   #configuration options
   region = "us-east-1"
 }
-
