c637bee24a7cb6980f7acb9baf7ca17a27bcad25
chaitra78 <53751398+chaitra78@users.noreply.github.com>
2025-09-15 15:19:08 +0530

    Update provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -15,8 +15,8 @@ terraform {
     bucket         = "my-s3-bucket-name-cr"
     key            = "usecase3/vpc/terraform.tfstate"
     region         = "us-east-1" # region of bucket and dynamodb
-    use_lockfile = true     #s3 by default supports locking but to be explicit
-    encrypt       = true
+  #  use_lockfile = true     #s3 by default supports locking but to be explicit
+  # encrypt       = true
   }
 }
 
