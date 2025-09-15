62c8a9d0bb684e0c10c2a7da17fcc79296324927
chaitra78 <53751398+chaitra78@users.noreply.github.com>
2025-09-15 15:53:22 +0530

    Update provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -13,7 +13,7 @@ terraform {
 }
   backend "s3" { #backend info should always be in terraform block
     bucket         = "my-s3-bucket-name-cr"
-    key            = "usecase3/vpc/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1" # region of bucket and dynamodb
   #  use_lockfile = true     #s3 by default supports locking but to be explicit
   # encrypt       = true
